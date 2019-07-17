a3d624c4c7d65e2dd44528dc9353f49028f8e820
Marius Mather <warsquid@gitlab.com>
2019-07-18 08:53:49 +1000

    Render all formats in publish script

--- publish_rstudio.R
+++ publish_rstudio.R
@@ -1,7 +1,7 @@
 knitr::write_bib(c(
   .packages(), 'bookdown', 'knitr', 'rmarkdown'
 ), 'packages.bib')
-bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")
+bookdown::render_book("index.Rmd", output_format = "all")
 bookdown::publish_book(
     name = "Rad",
     account = "####"
