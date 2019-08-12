b7508c2f716503607d57d135817dc58789ec33dc
warsquid <warsquid@gitlab.com>
2019-08-12 23:40:58 +1000

    Fix xcolor issues by manually replacing

--- publish_rstudio.R
+++ publish_rstudio.R
@@ -1,6 +1,16 @@
 knitr::write_bib(c(
   .packages(), 'bookdown', 'knitr', 'rmarkdown'
 ), 'packages.bib')
+
+# Load xcolor instead of color for pdf
+options(bookdown.post.latex = function(x) {
+    stringr::str_replace(
+        x,
+        stringr::fixed("\\usepackage{color}"), 
+        "\\usepackage[table]{xcolor}"
+    )
+})
+
 bookdown::render_book("index.Rmd", output_format = "all")
 bookdown::publish_book(
     name = "Rad",
